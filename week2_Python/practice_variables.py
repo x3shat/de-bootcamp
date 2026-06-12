5d040f2cc0d042262c8fbc15dc071a9898355dac
x3shat <shatantra@gmail.com>
2026-06-12 03:44:03 +0000

    week 2 python data types

---
+++ week2_Python/practice_variables.py
@@ -0,0 +1,15 @@
+name = "S Maji"
+city ="Kolkata"
+target_salary=750000
+print(f"My name is {name}.My city is {city}. My target salary is {target_salary}, which is {target_salary/100000:0.1f} LPA ")
+
+
+rows= 1247503 
+sec = 38
+print(f"Rows per second is {rows//sec}")
+
+
+print(17%5) #2
+print(17//5) #3
+print("5" + "3") # 53
+print(int("5") + 3) # 8
